6a2f9a1ec8e6e1e368122f28e8f98f2e9497736c
Cedric Williams <support@unlockscoreai.com>
2025-08-16 08:34:41 +0000

    File changes

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  projectId: "credit-clarity-ai-3xl8s",
-  appId: "1:925810683734:web:d0910772d7671868d4b0f7",
-  storageBucket: "credit-clarity-ai-3xl8s.appspot.com",
-  apiKey: "AIzaSyAas1vTLl8pq5eYuBiyg1Db-l6Nq2YplPQ",
-  authDomain: "credit-clarity-ai-3xl8s.firebaseapp.com",
-  messagingSenderId: "925810683734",
+  "projectId": "credit-clarity-ai-3xl8s",
+  "appId": "1:925810683734:web:d0910772d7671868d4b0f7",
+  "storageBucket": "credit-clarity-ai-3xl8s.firebasestorage.app",
+  "apiKey": "AIzaSyAas1vTLl8pq5eYuBiyg1Db-l6Nq2YplPQ",
+  "authDomain": "credit-clarity-ai-3xl8s.firebaseapp.com",
+  "messagingSenderId": "925810683734"
 };
 
 // Initialize Firebase
